e5c6e539aa018d9633cacbd14577e67d3ba43c74
Rodrigo Cruz <rodriikc@gmail.com>
2022-07-09 23:30:25 -0300

    Inicio proyecto servicio-productos

---
+++ practica-servicio-productos/src/main/java/com/practica/servicio/productos/PracticaServicioProductosApplication.java
@@ -0,0 +1,13 @@
+package com.practica.servicio.productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaServicioProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaServicioProductosApplication.class, args);
+	}
+
+}
---
+++ practica-servicio-productos/src/test/java/com/practica/servicio/productos/PracticaServicioProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.servicio.productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaServicioProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
