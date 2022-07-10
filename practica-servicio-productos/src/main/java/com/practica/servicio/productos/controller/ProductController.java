7e7f9b5cd620b341d63bb4205a43303d2e4111d5
Rodrigo Cruz <rodriikc@gmail.com>
2022-07-10 10:43:14 -0300

    servicio-productos: especifico el tipo de dato de retorno, de los métodos del controller

--- practica-servicio-productos/src/main/java/com/practica/servicio/productos/controller/ProductController.java
+++ practica-servicio-productos/src/main/java/com/practica/servicio/productos/controller/ProductController.java
@@ -3,13 +3,17 @@
  */
 package com.practica.servicio.productos.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.practica.servicio.productos.entity.Product;
 import com.practica.servicio.productos.service.IProductService;
 
 /**
@@ -24,13 +28,13 @@ public class ProductController {
     private IProductService service;
 
     @GetMapping
-    public ResponseEntity<?> listAll() {
-        return ResponseEntity.ok(service.findAll());
+    public ResponseEntity<List<Product>> listAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(service.findAll());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getById(@PathVariable Long id) {
-        return ResponseEntity.ok(service.findById(id));
+    public ResponseEntity<Product> getById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.findById(id));
     }
 
 }
