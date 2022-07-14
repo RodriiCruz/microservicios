d8f8285b8bab70ea1eb897f434a9770970959d65
Rodrigo Cruz <rodriikc@gmail.com>
2022-07-14 20:09:51 -0300

    Refactorizo para aplicar CircuitBreaker y TimeLimiter con anotaciones en lugar de inyectar CircuitBreakerFactory

--- practica-servicio-items/src/main/java/com/practica/servicio/items/controller/ItemController.java
+++ practica-servicio-items/src/main/java/com/practica/servicio/items/controller/ItemController.java
@@ -3,9 +3,10 @@
  */
 package com.practica.servicio.items.controller;
 
+import java.util.concurrent.CompletableFuture;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,9 @@ import com.practica.servicio.items.entity.Item;
 import com.practica.servicio.items.entity.Product;
 import com.practica.servicio.items.service.ItemService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+
 /**
  * 
  * @author Rodrigo Cruz <rodriikc@gmail.com>
@@ -23,9 +27,6 @@ import com.practica.servicio.items.service.ItemService;
 @RestController
 @RequestMapping // ("/items") ahora lo asigno mediante las configuraciones del gateway
 public class ItemController {
-
-    @Autowired
-    private CircuitBreakerFactory cbFactory;
 
     @Autowired
     @Qualifier("serviceFeign") // cumple la misma función que indicar @Primary en la clase implementada
@@ -36,18 +37,17 @@ public class ItemController {
         return ResponseEntity.ok(service.findAll());
     }
 
+    @CircuitBreaker(name = "items", fallbackMethod = "alternativeMethod")
+    @TimeLimiter(name = "items")
     @GetMapping("/{id}/{quantity}")
-    public ResponseEntity<?> getById(@PathVariable Long id, @PathVariable Integer quantity) {
-        return cbFactory.create("items") // id del cb que se va a implementar
-                .run(() -> ResponseEntity.ok(service.findById(id, quantity)), e -> alternativeMethod(id, quantity));
-        // dentro de este método nos tratamos de comunicar con el microservicio
-        // en caso de error, se ejecuta el fallback - Resiliencia programatica
+    public CompletableFuture<ResponseEntity<?>> getById(@PathVariable Long id, @PathVariable Integer quantity) {
+        return CompletableFuture.supplyAsync(() -> ResponseEntity.ok(service.findById(id, quantity)));
     }
 
-    public ResponseEntity<?> alternativeMethod(Long id, Integer quantity) {
+    public CompletableFuture<ResponseEntity<?>> alternativeMethod(Long id, Integer quantity, Throwable e) {
         Product product = Product.builder().id(id).name("Product XYZ").price(500.00).build();
         Item item = Item.builder().quantity(3).product(product).build();
 
-        return ResponseEntity.ok(item);
+        return CompletableFuture.supplyAsync(() -> ResponseEntity.ok(item));
     }
 }
