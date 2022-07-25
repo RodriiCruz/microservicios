6fc683fa002ced629456cc098a623c0cdcb8e95d
Rodrigo Cruz <rodriikc@gmail.com>
2022-07-25 14:29:59 -0300

    Se excluye la autoconfiguracion del data source para eliminar dependencia H2. Genero nuevo JAR.

--- practica-servicio-commons/src/main/java/com/practica/servicio/commons/PracticaServicioCommonsApplication.java
+++ practica-servicio-commons/src/main/java/com/practica/servicio/commons/PracticaServicioCommonsApplication.java
@@ -1,7 +1,10 @@
 package com.practica.servicio.commons;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 public class PracticaServicioCommonsApplication {
 }
