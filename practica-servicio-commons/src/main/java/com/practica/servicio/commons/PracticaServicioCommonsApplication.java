21fab1292e51259543ce0ef2a3b6465a25897ca6
Rodrigo Cruz <rodriikc@gmail.com>
2022-07-25 14:22:24 -0300

    Creo servicio commons para generar libreria y reutilizar clases

---
+++ practica-servicio-commons/src/main/java/com/practica/servicio/commons/PracticaServicioCommonsApplication.java
@@ -0,0 +1,7 @@
+package com.practica.servicio.commons;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaServicioCommonsApplication {
+}
---
+++ practica-servicio-commons/src/test/java/com/practica/servicio/commons/PracticaServicioCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.servicio.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaServicioCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
